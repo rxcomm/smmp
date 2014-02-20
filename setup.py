eafdb7373dcb7ea1e346d965c8b359422e3f893f
David R. Andersen <k0rx@RXcomm.net>
2014-02-19 18:07:31 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+import zipfile
+import os
+import pwd
+
+setup(name='smmp',
+      version='0.2.1',
+      description='Python implementation of the SMMP protocol',
+      author='David R. Andersen',
+      url='none',
+      py_modules=['smmp'],
+      install_requires=['python-gnupg >= 0.3.5', 'passlib >= 1.6.1'],
+     )
