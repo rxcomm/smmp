ecb8746243c39875f3ebfccd1f8cd58858b1a3e0
David R. Andersen <k0rx@RXcomm.net>
2014-03-02 10:12:57 -0600

    added resync to test.py

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 from smmp import Organizer, Participant, BummerUndecryptable, BadHMAC
+from curve25519 import keys
+from passlib.utils.pbkdf2 import pbkdf2
 from random import randint
 from time import sleep
 from copy import deepcopy
 import os
 import binascii
+import hashlib
+
+def strxor(s0, s1):
+    l = [chr(ord(a)^ord(b)) for a,b in zip(s0, s1)]
+    return ''.join (l)
 
 def hilite(text, c=False):
     attr = []
@@ -99,7 +106,9 @@ p7.initState('my cool group name',
 
 participants = ('p0', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7')
 data_old = deepcopy(p0.state)
+counter = 0
 while True:
+    counter += 1
     try:
         os.system('clear')
         print '\x1b[;32m Ratchet Keys\x1b[0m'
@@ -122,6 +131,8 @@ while True:
                     pass
                 elif key == 'my_index':
                     pass
+                elif key == 'digest':
+                    print 'dgt: '+hilite(binascii.b2a_base64(p0.state[key]).strip(), chg)
                 else:
                     print '{:>3}'.format(key)+': '+hilite(binascii.b2a_base64(p0.state[key]).strip(), chg)
                 chg = False
@@ -130,26 +141,63 @@ while True:
         data_old = deepcopy(p0.state)
 
         encrypter = randint(0,7)
+
+        if counter % 5 == 0:
+            exec('v, V = p' + str(encrypter) + '.genKey()')
+            r = {}
+            R = {}
+            for i in range(len(participants)):
+                key = keys.Private(secret=hashlib.sha256(str(i).zfill(32)).digest())
+                r[i] = key.private
+                R[i] = key.get_public().serialize()
+            DHR = '\x00' * 32
+            for i in range(len(participants)):
+                DHR = strxor(DHR, R[i])
+            exec('RK = hashlib.sha256(DHR + p' + str(encrypter) + '.genDH(v, DHR)).digest()')
+            HK = pbkdf2(RK, b'\x01', 10, prf='hmac-sha256')
+            NHK = pbkdf2(RK, b'\x02', 10, prf='hmac-sha256')
+            MK = pbkdf2(RK, b'\x03', 10, prf='hmac-sha256')
+            for i in range(len(participants)):
+                exec('p' + str(i) + '.ratchetKey = r[i]')
+                exec('p' + str(i) + '.state["v"] = deepcopy(v)')
+                exec('p' + str(i) + '.state["R"] = deepcopy(R)')
+                exec('p' + str(i) + '.state["RK"] = deepcopy(RK)')
+                exec('p' + str(i) + '.state["HK"] = deepcopy(HK)')
+                exec('p' + str(i) + '.state["NHK"] = deepcopy(NHK)')
+                exec('p' + str(i) + '.state["MK"] = deepcopy(MK)')
+            # exec() doesn't like null bytes
+            p0.state['digest'] = '\x00' * 32
+            p1.state['digest'] = '\x00' * 32
+            p2.state['digest'] = '\x00' * 32
+            p3.state['digest'] = '\x00' * 32
+            p4.state['digest'] = '\x00' * 32
+            p5.state['digest'] = '\x00' * 32
+            p6.state['digest'] = '\x00' * 32
+            p7.state['digest'] = '\x00' * 32
+
+
         print
         print '\x1b[;32m Message encrypted by P'+str(encrypter)+'\x1b[0m'
-        print '--------------------------------------------------'
-        command = 'ciphertexts = p'+str(encrypter)+'.encrypt("This message was decrypted successfully")'
+        print '---------------------------------------------------------'
+        command = 'ciphertexts = p'+str(encrypter)+'.encrypt("This message was encrypted by P" + str(encrypter))'
         exec(command)
 
         for i in range(len(participants)):
-            try_twice = randint(0,9)
+            #try_twice = randint(0,9)
+            try_twice = 1
             second_decryption = False
             try:
-                print 'P'+str(i)+': '+eval(participants[i]+'.decrypt(ciphertexts[i][3:])')
+                print 'P'+str(i)+': '+eval(participants[i]+'.decrypt(ciphertexts[i][3:])') + ' and decrypted by P' + str(i)
                 if try_twice == 0:
                     second_decryption = True
                     print ' ' + eval(participants[i]+'.decrypt(ciphertexts[i][3:])')
-            except (BummerUndecryptable, BadHMAC):
+            #except (BummerUndecryptable, BadHMAC):
+            except BadHMAC:
                 if not second_decryption:
                     print '* P'+str(i)+': Decryption Error!'
                 else:
                     print '* P'+str(i)+': Error Decrypting Second Time!'
-        print '--------------------------------------------------'
+        print '---------------------------------------------------------'
         print
         print '\x1b[;32mNotes:\x1b[0m'
         print '\x1b[;32m1. Forward secrecy implies that no one can decrypt previous messages.\x1b[0m'
